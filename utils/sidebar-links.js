25ac7a3d986861cfa8bd85164b246f5a7f10bc9e
Samuel Dushimimana <66276301+dushimsam@users.noreply.github.com>
2021-12-03 10:13:17 +0200

    removed updates

--- utils/sidebar-links.js
+++ utils/sidebar-links.js
@@ -131,18 +131,19 @@ export const adminLinks = [
                     fill="rgba(112,112,112,1)"/>
             </svg>
         )
-    },
-    {
-        name: "App updates",
-        href: "/admin/app-updates",
-        icon: (
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18">
-                <path fill="none" d="M0 0h24v24H0z"/>
-                <path fill="rgba(112,112,112,1)"
-                      d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm4.82-4.924A7 7 0 0 0 9.032 5.658l.975 1.755A5 5 0 0 1 17 12h-3l2.82 5.076zm-1.852 1.266l-.975-1.755A5 5 0 0 1 7 12h3L7.18 6.924a7 7 0 0 0 7.788 11.418z"/>
-            </svg>
-        )
-    },
+    }
+    ,
+    // {
+    //     name: "App updates",
+    //     href: "/admin/app-updates",
+    //     icon: (
+    //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18">
+    //             <path fill="none" d="M0 0h24v24H0z"/>
+    //             <path fill="rgba(112,112,112,1)"
+    //                   d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm4.82-4.924A7 7 0 0 0 9.032 5.658l.975 1.755A5 5 0 0 1 17 12h-3l2.82 5.076zm-1.852 1.266l-.975-1.755A5 5 0 0 1 7 12h3L7.18 6.924a7 7 0 0 0 7.788 11.418z"/>
+    //         </svg>
+    //     )
+    // },
     {
         name: "FeedBacks",
         href: "/admin/feedbacks",
